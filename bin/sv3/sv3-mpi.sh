9d449a2dad0e8177a589f88319ec5d8d74349079
mattkwiecien <mattkwiecien@gmail.com>
2023-06-30 08:10:11 -0700

    Fixing a file name issue

--- bin/sv3/sv3-mpi.sh
+++ bin/sv3/sv3-mpi.sh
@@ -6,7 +6,7 @@ ncores=$2
 # Source the base env variables 
 # This is overwriting the docker container global variables with your local repos/paths
 
-source $LEGACYHALOS_CODE_DIR/bin/sv3/sv3-env
+source $LEGACYHALOS_CODE_DIR/bin/sv3/sv3-env.sh
 
 if [ $stage = "coadds" ]; then
     time python $LEGACYHALOS_CODE_DIR/bin/sv3/sv3-mpi.py --coadds --nproc $ncores --mpi --fname sv3_matches.fits --verbose 
