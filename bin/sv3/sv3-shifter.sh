5a262f5103e84d4419650cf330e40503e2dafff9
Matt Kwiecien <mattkwiecien@gmail.com>
2023-06-30 10:48:16 -0700

    Adding a new step to override the legacypipe mpi installation then installing mpi4py

--- bin/sv3/sv3-shifter.sh
+++ bin/sv3/sv3-shifter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 project=sv3
 
-echo 'Updating and loading the shifter image docker:legacysurvey/legacyhalos:v1.2'
+echo 'Updating and loading the shifter image docker:mattkwiecien/legacyhalos:v1.3.1'
 echo 'Load the environment with: '
 echo 'source '$LEGACYHALOS_CODE_DIR'/bin/sv3/sv3-env'
 
-shifterimg pull docker:legacysurvey/legacyhalos:v1.2
+shifterimg pull docker:mattkwiecien/legacyhalos:v1.3.1
 shifter /bin/bash 
